61a562440dd68dc5b0f5b6dadf44b6a69cff85ab
youthPaul <2889445151@qq.com>
2025-03-23 10:45:29 +0800

    solution for Lab7 task2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET];
+
 double
 now()
 {
@@ -40,6 +42,8 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&locks[i]);
+
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,6 +57,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -114,6 +120,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+
+  /* init locks */
+  for(int i = 0; i < NBUCKET; ++i) pthread_mutex_init(&locks[i], NULL);
 
   //
   // first the puts
