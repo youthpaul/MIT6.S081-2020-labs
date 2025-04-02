696b3bb9f84e7a8a63694e79237e24b88f7a2199
youthPaul <2889445151@qq.com>
2025-04-02 13:06:27 +0800

    solution for Lab9 bigfile

--- kernel/fs.c
+++ kernel/fs.c
@@ -401,6 +401,28 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
+  bn -= NINDIRECT;
+  if(bn < NDINDIRECT){ // double indirect inode
+    if((addr = ip->addrs[NDIRECT + 1]) == 0)
+      ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    uint dbn = bn / NINDIRECT;
+    if((addr = a[dbn]) == 0){
+      a[dbn] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    bp = bread(ip->dev, addr); // data block
+    a = (uint*)bp->data;
+    if((addr = a[bn % NINDIRECT]) == 0){
+      a[bn % NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
+
   panic("bmap: out of range");
 }
 
@@ -430,6 +452,26 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if(ip->addrs[NDIRECT + 1]){
+    struct buf* dbp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint*)dbp->data;
+    for(j = 0; j < NINDIRECT; j++){
+      if(a[j]){
+        bp = bread(ip->dev, a[j]);
+        uint* b = (uint*)bp -> data;
+        for(int k = 0; k < NINDIRECT; ++k)
+          if(b[k])
+            bfree(ip->dev, b[k]);
+        bfree(ip->dev, a[j]);
+        brelse(bp);
+        a[j] = 0;
+      }
+    }
+    brelse(dbp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
